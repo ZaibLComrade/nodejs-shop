cd97d958b7a55935245e256645415581e353a64e
ZaibLComrade <kmjahanzaib@proton.me>
2024-05-21 19:45:43 +0600

    Added interface and zod validation schema for orders

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import productRoute from "./app/modules/product/product.route";
 import cors from "cors";
 
@@ -7,13 +7,37 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/products", productRoute)
+app.use("/api/products", productRoute);
+
+// Global error handler
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+	try {
+		res.status(400).json({
+			succcess: false,
+			message: (error?.message as string) || "Something went wrong",
+			error,
+		});
+	} catch (err) {
+		res.send(err);
+	}
+	next();
+});
+
+// Handles all requsts for all path (Not Found)
+app.all("*", (req: Request, res: Response) => {
+	res.status(404).json({
+		path: req.url,
+		method: req.method,
+		message: "Not Found",
+	});
+});
 
 // Checks server health
-app.all("/health", async(req: Request, res: Response) => {
+app.all("/health", (req: Request, res: Response) => {
 	res.status(200).json({
-		message: "Server is running"
-	})
-})
+		message: "Server is running",
+	});
+});
 
 export default app;
