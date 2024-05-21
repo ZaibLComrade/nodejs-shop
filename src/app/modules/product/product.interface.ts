097791091a5673a13539ee429aaaab795c46a087
ZaibLComrade <kmjahanzaib@proton.me>
2024-05-21 15:56:17 +0600

    Added product interface

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,15 @@
+export interface IProduct {
+	name: string;
+	description: string;
+	price: number;
+	category: string,
+	tags: [string],
+	variant: [{
+		type: string;
+		value: string;
+	}]
+	inventory: {
+		quantity: number;
+		inStock: boolean;
+	}
+}
