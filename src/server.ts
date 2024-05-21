aede40c4d1313d5ca53cac5f5967d72581c2355e
ZaibLComrade <kmjahanzaib@proton.me>
2024-05-21 15:48:23 +0600

    Connected with MongoDB

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,13 @@ import config from "./config";
 import { connect } from "mongoose";
 
 const main = async () => {
+	try {
+		await connect(config.db_uri as string, { dbName: config.db_name });
+		console.info("==== MongoDB Connected ====")
+	} catch (err) {
+		console.error(err);
+	}
+	
 	app.listen(config.port || 5000, () => {
 		console.log(`Server is running on http://localhost:${config.port}`);
 	})
